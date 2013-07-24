ad5b855e022ef61eb4d198d26c7f17fbfd2c62ca
Esteban Soler <soleresteban@hotmail.com>
2013-07-24 15:36:33 -0300

    active admin install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mendezcollado::Application.config.secret_token = 'de1b677e1b9bff09e2968f4fd3d1565ef47942185bd4eb9d66691dc1201fad13cfb2b04a813f27aca95a3684f440ecfc0b472e4b6e9da5b7583d8511ef48a0e3'
